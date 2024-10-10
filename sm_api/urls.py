5c01505af99be1b4ca5faace9a5a4dd00d19d6c7
Linus Johansson <golfarenlj@gmail.com>
2024-10-10 23:59:55 +0000

    Refactor

--- sm_api/urls.py
+++ sm_api/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path(
-        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
+        'dj-rest-auth/registration/',
+        include('dj_rest_auth.registration.urls'),
     ),
     path('', include('core.urls')),
 ]
