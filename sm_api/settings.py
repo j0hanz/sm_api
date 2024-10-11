64c4588d4f3b59ae61b9a7cbb53d7c67629d40fb
Linus Johansson <golfarenlj@gmail.com>
2024-10-11 00:01:04 +0000

    Sorted code for improve readability

--- sm_api/settings.py
+++ sm_api/settings.py
@@ -9,15 +9,12 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-# Get the secret key from environment variables
-SECRET_KEY = os.getenv('SECRET_KEY')
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Get the debug mode from environment variables
+# Security settings
+SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
-
 ALLOWED_HOSTS = [
     '.gitpod.io',
     '127.0.0.1',
@@ -25,26 +22,7 @@ ALLOWED_HOSTS = [
     '.herokuapp.com',
 ]
 
-# CORS
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN'),
-        os.environ.get('CLIENT_ORIGIN_DEV'),
-    ]
-else:
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        r'^https://.*\.codeinstitute-ide\.net$',
-    ]
-
-CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_ALLOW_ALL = True
-
-# Cloudinary settings
-CLOUDINARY_STORAGE = {'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')}
-MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-# Installed apps
+# Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -52,23 +30,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'dj_rest_auth',
-    'cloudinary_storage',
     'django.contrib.sites',
-    'cloudinary',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'dj_rest_auth.registration',
+    'cloudinary',
+    'cloudinary_storage',
     'corsheaders',
+    'dj_rest_auth',
+    'dj_rest_auth.registration',
+    'rest_framework',
+    'rest_framework.authtoken',
     'core',
 ]
 
 SITE_ID = 1
 
-# Middleware
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
@@ -101,7 +78,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sm_api.wsgi.application'
 
-# Database
+# Database configuration
 if 'DEV' in os.environ:
     DATABASES = {
         'default': {
@@ -114,28 +91,19 @@ else:
         'default': dj_database_url.config(default=os.getenv('DATABASE_URL')),
     }
 
-# CSRF trusted origins
-CSRF_TRUSTED_ORIGINS = [
-    'https://*.codeinstitute-ide.net',
-    'https://*.herokuapp.com',
-]
-
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'
+    },
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'
     },
 ]
-
 
 # Internationalization settings
 LANGUAGE_CODE = 'en-us'
@@ -143,9 +111,17 @@ TIME_ZONE = 'Europe/Stockholm'
 USE_I18N = True
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
+
+# Media files (Uploaded files)
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+# Cloudinary settings
+CLOUDINARY_STORAGE = {
+    'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL'),
+}
 
 # Django REST framework configuration
 REST_FRAMEWORK = {
@@ -160,6 +136,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 12,
     'DATETIME_FORMAT': '%a %d-%m-%Y %H:%M',
 }
+
 if 'DEV' not in os.environ:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
         'rest_framework.renderers.JSONRenderer',
@@ -175,6 +152,26 @@ REST_AUTH = {
     'JWT_AUTH_SAMESITE': 'None',
     'USER_DETAILS_SERIALIZER': 'sm_api.serializers.CurrentUserSerializer',
 }
+
+# CORS settings
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN'),
+        os.environ.get('CLIENT_ORIGIN_DEV'),
+    ]
+else:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r'^https://.*\.codeinstitute-ide\.net$',
+    ]
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+
+# CSRF trusted origins
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.codeinstitute-ide.net',
+    'https://*.herokuapp.com',
+]
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
